43542e2363af267be49cf032b6e4ecbe4388822c
Lanrecode23 <lanrecode23@gmail.com>
2024-09-06 13:08:09 +0100

    'added'

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>      
     </>
