be25e218206a13a892de5b58adab28f12d1f8e85
Lanrecode23 <lanrecode23@gmail.com>
2024-09-13 11:19:11 +0100

    commited

--- src/components/TransactionEvent.jsx
+++ src/components/TransactionEvent.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import useGetTransaction from '../hooks/useGetTransaction'
 
 function TransactionEvent() {
-  const { transactionsTotal}= useGetTransaction()
-  const{balance, expense, income} = transactionsTotal
+  const { transactionsTotal } = useGetTransaction()
+  const { balance, expense, income } = transactionsTotal
   return (
     <div className='mt-4'>
       <div className="balance">
